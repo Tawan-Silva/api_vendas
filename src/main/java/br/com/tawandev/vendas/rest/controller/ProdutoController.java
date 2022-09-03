0622dc3948f58b0f5f303288b0ce95df8b3f0a97
Tawan Silva <tawan.tls43@gmail.com>
2022-09-02 22:42:03 -0300

    produto controller

---
+++ src/main/java/br/com/tawandev/vendas/rest/controller/ProdutoController.java
@@ -0,0 +1,65 @@
+package br.com.tawandev.vendas.rest.controller;
+
+import br.com.tawandev.vendas.domain.entities.Cliente;
+import br.com.tawandev.vendas.domain.entities.Produto;
+import br.com.tawandev.vendas.repositories.ProdutosRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/produtos")
+public class ProdutoController {
+
+    @Autowired
+    private ProdutosRepository produtosRepository;
+
+    @GetMapping("/{id}")
+    public Produto getById(@PathVariable Integer id) {
+        return produtosRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "Produto não encontrado"));
+    }
+
+    @GetMapping
+    public List<Produto> find(Produto filtro) {
+        ExampleMatcher matcher = ExampleMatcher
+                .matching()
+                .withIgnoreCase()
+                .withStringMatcher(
+                        ExampleMatcher.StringMatcher.CONTAINING);
+
+        Example example = Example.of(filtro, matcher);
+        return produtosRepository.findAll(example);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Produto save(@RequestBody Produto produto) {
+        return produtosRepository.save(produto);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@PathVariable Integer id, @RequestBody Produto produto) {
+        produtosRepository.findById(id).map(p -> {
+            produto.setId(p.getId());
+            produtosRepository.save(produto);
+            return produto;
+        }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto não encontrado"));
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Integer id) {
+        produtosRepository.findById(id).map(p -> {
+            produtosRepository.save(p);
+            return Void.TYPE;
+        }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto não encontrado"));
+    }
+}
