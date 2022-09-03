157bc3de90746b66cd64151d6d2b1fdcc92a5f6c
Tawan Silva <tawan.tls43@gmail.com>
2022-09-02 22:36:49 -0300

    entity Pedido

---
+++ src/main/java/br/com/tawandev/vendas/domain/entities/Pedido.java
@@ -0,0 +1,48 @@
+package br.com.tawandev.vendas.domain.entities;
+
+import br.com.tawandev.vendas.domain.enums.StatusPedido;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "pedido")
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+
+    @Column(name = "data_pedido")
+    private LocalDate dataPedido;
+
+    @Column(name = "total", precision = 20, scale = 2)
+    private BigDecimal total;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private StatusPedido status;
+
+    @OneToMany(mappedBy = "pedido")
+    private List<ItemPedido> itens;
+
+    public List<ItemPedido> getItens() {
+        if (this.itens == null) {
+            this.itens = new ArrayList<>();
+        }
+        return this.itens;
+    }
+}
